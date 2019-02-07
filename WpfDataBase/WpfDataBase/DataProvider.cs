193a704a069326e4ceed157a8c9e40c87a0c3271
Wik Pac <victoor@o2.pl>
2019-02-07 09:44:54 +0100

    DbLayer

---
+++ WpfDataBase/WpfDataBase/DataProvider.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfDataBase
+{
+    public class DataProvider
+    {
+        SqlConnection _connection;
+        string _connectString;
+        public DataProvider()
+        {
+            _connectString = ConfigurationManager.ConnectionStrings["ConnectStringKundenDB"].ConnectionString;
+        }
+
+        public void FillDataGrid(DataGrid grid, string sqlCommand, bool isStoredProcedure = false)
+        {
+            _connection = new SqlConnection(_connectString);
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, _connection);
+
+                //if(isStoredProcedure)
+                //    cmd.CommandType = CommandType.StoredProcedure;
+
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable("Customers");
+
+                sda.Fill(dt);
+
+                grid.ItemsSource = dt.DefaultView;
+            }
+
+        }
+
+        public string JsonConvertText(string sqlCommand, bool isStoredProcedure = false)
+        {
+            var indexes = new List<ushort>();
+            if (isStoredProcedure)
+                indexes.AddRange(new ushort[3] { 1, 4, 5 });
+            else
+                indexes.AddRange(new ushort[3] { 0, 1, 2 });
+
+
+            List<Person> persons = new List<Person>();
+
+            _connection = new SqlConnection(_connectString);
+            using (_connection)
+            {
+                _connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlCommand, _connection);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                int i = 0;
+                while (reader.Read())
+                {
+                    if (reader[i].ToString() == string.Empty)
+                        break;
+
+                    var person = new Person();
+                    person.Name = reader[indexes[0]].ToString();
+                    person.City = reader[indexes[1]].ToString();
+                    person.Zip = (int)reader[indexes[2]];
+                    persons.Add(person);
+                }
+                reader.Close();
+            }
+
+            string json = JsonConvert.SerializeObject(persons);
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "jsonTest.json"), json);
+
+            return json;
+        }
+    }
+}
--- WpfDataBase/WpfDataBase/MainWindow.xaml.cs
+++ WpfDataBase/WpfDataBase/MainWindow.xaml.cs
@@ -14,78 +14,22 @@ namespace WpfDataBase
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataProvider _dataProvider;
         public MainWindow()
         {
             InitializeComponent();
-            FillDataGrid("SELECT CustomerName, City, PostalCode FROM Data");
-        }
-        private void FillDataGrid(string sqlCommand, bool isStoredProcedure = false)
-        {
+            _dataProvider = new DataProvider();
 
-            string ConString = ConfigurationManager.ConnectionStrings["ConnectStringKundenDB"].ConnectionString;
-
-            using (SqlConnection con = new SqlConnection(ConString))
-            {
-                SqlCommand cmd = null;
-
-
-                cmd = new SqlCommand(sqlCommand, con);
-                //if(isStoredProcedure)
-                //    cmd.CommandType = CommandType.StoredProcedure;
-
-
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-
-                DataTable dt = new DataTable("Customers");
-
-                sda.Fill(dt);
-
-                grdEmployee.ItemsSource = dt.DefaultView;
-
-                JsonConvertText(con, cmd, isStoredProcedure);
-            }
-
-        }
-
-        private void JsonConvertText(SqlConnection con, SqlCommand cmd, bool isStoredProcedure = false)
-        {
-
-            var indexes = new List<ushort>();
-            if (isStoredProcedure)
-                indexes.AddRange(new ushort[3] { 1, 4, 5 });
-            else
-                indexes.AddRange(new ushort[3] { 0, 1, 2 });
-
-
-
-            con.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            List<Person> persons = new List<Person>();
-
-            int i = 0;
-            while (reader.Read())
-            {
-                if (reader[i].ToString() == string.Empty)
-                    break;
-
-                var person = new Person();
-                person.Name = reader[indexes[0]].ToString();
-                person.City = reader[indexes[1]].ToString();
-                person.Zip = (int)reader[indexes[2]];
-                persons.Add(person);
-            }
-            reader.Close();
-            con.Close();
-
-            string json = JsonConvert.SerializeObject(persons);
-            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "jsonTest.json") , json);
-            labJson.Content = json;
+            string sqlCommand = "SELECT CustomerName, City, PostalCode FROM Data";
+            _dataProvider.FillDataGrid(grdEmployee, sqlCommand);
+            _dataProvider.JsonConvertText(sqlCommand, false);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string storedProcedure = $"EXEC SelectCustomersFromCity @City = '{txtCity.Text}'";
-            FillDataGrid(storedProcedure, true);
+            _dataProvider.FillDataGrid(grdEmployee, storedProcedure);
+            _dataProvider.JsonConvertText(storedProcedure, true);
         }
     }
 }
